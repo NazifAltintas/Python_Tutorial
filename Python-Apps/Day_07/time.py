e96a5b17f75997292ce2760489eb083484ce6fb2
Nazif <nazifltnts@gmail.com>
2022-12-16 23:43:02 +0100

    e1

--- Python-Apps/Day_07/time.py
+++ Python-Apps/Day_07/time.py
@@ -5,9 +5,9 @@ days = given_seconds//(24*60*60)
 seconds_1 = given_seconds % (24*60*60)    # remaining seconds after we get days
 # Calculate the hours
 hours = seconds_1//(60*60)          # get the hours out of seconds_1
-seconds_2 = seconds_1 % (60*60)     # remaining seconds after we get hours
+seconds_2 = seconds_1 % (60*60)     # remaining seconds after we get hour
 # Calculate the minutes
-minutes = seconds_2 // 60           # get the minutes
+minutes = seconds_2 // 60           # get the minutes 
 # Finally calculate the remaining seconds after we get the minutes
 seconds = seconds_2 % 60
 print(days, "days", hours, "hours", minutes, "minutes", seconds, "seconds")
